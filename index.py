e7eeb00d5b394454458c93f7c640a525765bdf37
alyosha_84 <77863290+tschultz84@users.noreply.github.com>
2022-01-21 07:14:56 -0700

    1/21/22 update
    Have the Flask page update working.

--- index.py
+++ index.py
@@ -5,8 +5,8 @@ Created on Sat Jan 15 06:44:28 2022
 @author: 14154
 """
 #LOad up FLASK and the HTML templates.
-from flask import Flask
-from flask import render_template
+#from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 #You have to direct the FLask application to the right place to load. 
 flask_dir = "C:\\Users\\14154\\OneDrive\\Python\\climate_mapper\\python\\climate_flask_page\\templates\\"
@@ -28,13 +28,25 @@ that follows it. A common pattern with decorators is to use them to register
  associate the URLs / and /index to this function. This means that 
  when a web browser requests either of these two URLs, Flask is going 
  to invoke this function and pass the return value of it back to the browser as a response."""
+#Index is the homepage wher data is entered.
 @app.route('/index')
 @app.route('/')
 def index():
+    return render_template('index.html')
+    #This is the output page.
+    
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+    #This one works for sure.
+    #return render_template('output.html', lat=request.form['latitude'])
+    return render_template('output.html', lat=request.form['latitude'], long=request.form['longitude'])
+#    return render_template('output.html', long=request.form['longitude'])
+    #return render_template('output.html')
    
-   return render_template('index.html')
+   
 if __name__ == '__main__':
   # app.run()
   # app.debug = True
    app.run()
    #app.run(debug = True)
+
