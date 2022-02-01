e501f758bd4ae06c550dfcfa41772e2a44a08a3c
alyosha_84 <77863290+tschultz84@users.noreply.github.com>
2022-02-01 06:16:47 -0700

    2/1/2022 update

--- index.py
+++ index.py
@@ -45,8 +45,8 @@ def output():
     #Assign variables to pass.
     lat1=float(request.form['latitude'])
     long1=float(request.form['longitude'])
-    bdate1="1-1-2021"
-    edate1="1-31-2021"
+    bdate1="1-1-2011"
+    edate1="12-31-2021"
     data=load.LoadStation([lat1,long1],True) #Loading in Bozeman, MT coordinates
     
     #Pulls the interesting station metadata.
@@ -77,14 +77,44 @@ def output():
     #A string describing the baseline period.
     basedesc = calc.base_period_string
     alphavalue = str(100*calc.yaml['ALPHA'])+"%"
+    #Ref and Baseline data points. 
+    abs_temp =[round(calc.refmean,1),round(calc.basemean,1)]
+    
     #Difference between temperature inr eference and baseline.
     refdelta = calc.ref_base_delta
     #P value of difference betwen them.
-    pvalue = str(round(100*calc.ref_pvalue,5))+"%"
+    pvalue = str(round(100*calc.ref_pvalue,2))+"%"
     #And statistical signitfance.
     refsig = calc.ref_stat_sig 
-        
     
+    #TREND RELATED DATA
+    #The number of years in the recent trend.
+    trendyears=calc.yaml['RECENT_TREND_YEARS']
+    #The warming trend, in Farenheight degrees per decde.
+    trendF = calc.trend_data_str
+    #The trend's p-value and whether it is statistically significant.
+    trend_p= str(round(100*calc.trend_p ,2))+"%"
+    real_trend = calc.trend_is_real
+        
+    ##THese are key values to return out of the function.
+    alltimestr = calc.alltimestr #A string describing all the weather station dates
+    whole_mean = calc.whole_mean #Average temperature ove entire record
+    whole_max_info = calc.whole_max_info #of form - maxium temperature, whole record, maxdate [whole_tmax,maxdate]
+    whole_min_info = calc.whole_min_info # min temp whole record, mindate[ whole_tmin,mindate]
+    ref_max_info = calc.ref_max_info # ref max temp, datte [ref_max,rmaxdate]
+    ref_min_info =  calc.ref_min_info # ref min temp, date [ref_min,rmindate]
+    
+    gen_info = [
+               "Station Name: "+str(name1),
+               "Station ID: "+str(stid),
+               "Period of Time Covered by Weather Station Data: "+alltimestr,
+               "Average Temperature (TMID) over this Period (F): "+str(round(whole_mean,2)),
+               "Maximum Temperature over this Period (F), and date(s): "+str(round(whole_max_info[0],2))+" in "+str(whole_max_info[1]),
+               "Minimum Temperature over this Period (F), and date(s): "+str(round(whole_min_info[0],2))+" in "+str(whole_min_info[1])
+               ]
+               
+               
+               #"Distance from Ref Point: "+str(round(stdist,1))+" miles"]
     
     #name1="Bozeman"
     #return render_template('output.html', lat=lat1, long=long1,bdate=bdate1,edate=edate1,stname=name1)
@@ -96,7 +126,12 @@ def output():
                            #And, some other relevant metadata about base period.
                            lastbaseyear=lastbaseyear,veryfirstyear=veryfirstyear,
                            #Then, data comparing reference to baseline.
-                           alphavalue = alphavalue,refdelta=refdelta,pvalue=pvalue,basedesc=basedesc,refsig=refsig
+                           alphavalue = alphavalue,refdelta=refdelta,pvalue=pvalue,basedesc=basedesc,refsig=refsig,
+                           abs_temp=abs_temp,
+                           #Trend related data.
+                           trend_p=trend_p,real_trend=real_trend,trendF=trendF,trendyears=trendyears,
+                           #General climate related data
+                           gen_info=gen_info
                            )
 
 
